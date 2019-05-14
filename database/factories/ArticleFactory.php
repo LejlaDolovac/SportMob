ecb881241783eea9cfdbf12daf6102c4ed3ac9f0
Schavon <Alexandra.schavon@hotmail.com>
2019-05-14 15:29:59 +0200

    API in progress

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Article;
+use Faker\Generator as Faker;
+
+$factory->define(Article::class, function (Faker $faker) {
+    return [
+      'title' => $faker->name,
+      'text' => $faker->realText($maxNbChars = 3000, $indexSize = 2), //max chars
+      'img' => $faker->imageUrl($width = 640, $height = 480, 'sports', true, 'Faker', true),
+      'author' => $faker->name,
+      'category' => $faker->randomElement($array = array ('Fotbal','Basketbal','Basketbal')), // create categorys.
+      'rank' => $faker->numberBetween($min = 1, $max = 3) // ranks the news from high to low.
+
+    ];
+});
