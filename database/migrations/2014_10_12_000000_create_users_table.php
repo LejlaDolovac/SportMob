89e7c9d172e85c864e84d44131bf793ce47d9048
Mattiasr <r.o.n.g.e@hotmail.com>
2019-05-20 13:12:52 +0200

    html code and css, Homepage

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -5,12 +5,12 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
-{
+
     /**
      * Run the migrations.
      *
      * @return void
-     */x
+     */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,32 +8,16 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-
-
-
-
-
-
-
-
-
-
-
-
+        <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
         <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: 'Nunito', sans-serif;
+                font-family: 'Montserrat', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
             }
 
             .position-ref {
@@ -67,7 +51,463 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            @import "compass/css3";
+ body {
+	 color: #404040;
+	 font: 100 1em/150% "proxima-nova", Helvetica, sans-serif;
+}
+ .wrapper {
+	 max-width: 1200px;
+	 margin: auto;
+}
+ h1, h2, h3, h4 {
+	 font-weight: 900;
+	 color: #333;
+	 margin: 1.5em 0 0.5em;
+}
+ h1 {
+	 font-size: 2em;
+}
+ h2 {
+	 font-size: 1.8em;
+}
+ h3 {
+	 font-size: 1.5em;
+}
+ h4 {
+	 font-size: 1.3em;
+}
+ strong {
+	 font-weight: 600;
+}
+ hr {
+	 border: none;
+	 height: 1px;
+	 background-color: rgba(51, 153, 204, .2);
+}
+
+/*Basic Grid Styles*/
+ .Grid {
+	 display: flex;
+	 flex-flow: row;
+	 flex-wrap: wrap;
+}
+ .u-textCenter {
+	 text-align: center;
+}
+ .Grid-cell {
+	 flex: 1;
+}
+ .Demo {
+	 padding: 0.8em 1em 0;
+	 margin-bottom: 1em;
+	 background: grey;
+	 transition: background-color 0.3s ease;
+	 border: 1px solid #3cc;
+	 border-radius: 3px;
+}
+ .Demo:after {
+	 content: "";
+	 display: block;
+	 margin-top: 0.8em;
+	 height: 1px;
+}
+ .Demo:hover {
+	 background: rgba(51, 153, 204, .6);
+}
+ .Demo.Holly {
+	 background: rgba(102, 51, 255, 0.1);
+}
+ .Demo.Holly:hover {
+	 background: rgba(102, 51, 255, 0.25);
+}
+/* With gutters*/
+ .Grid--gutters {
+	 margin-left: -1em;
+}
+ .Grid--gutters .Grid-cell {
+	 padding-left: 1em;
+}
+ .Grid--gutters .Grid--nested .Grid-cell:first-of-type .Demo {
+	 margin-right: 1em;
+}
+/* Justify per row*/
+ .Grid--right {
+	 justify-content: flex-end;
+}
+ .Grid--center {
+	 justify-content: center;
+}
+/* Alignment per row */
+ .Grid--top {
+	 align-items: flex-start;
+}
+ .Grid--bottom {
+	 align-items: flex-end;
+}
+ .Grid--center {
+	 align-items: center;
+}
+/* Alignment per cell */
+ .Grid-cell--top {
+	 align-self: flex-start;
+}
+ .Grid-cell--bottom {
+	 align-self: flex-end;
+}
+ .Grid-cell--center {
+	 align-self: center;
+}
+
+
+
+/*===========================================*/
+/* Base classes for all media - Mobile first */
+ .Grid--cols-2 > .Grid-cell {
+	 flex: 0 0 100%;
+}
+ .Grid--cols-3 > .Grid-cell {
+	 flex: 0 0 100%;
+}
+ .Grid--cols-4 > .Grid-cell {
+	 flex: 0 0 100%;
+}
+ .Grid--cols-6 > .Grid-cell {
+	 flex: 0 0 calc(50% - 1em);
+}
+ .Grid--cols-12 > .Grid-cell {
+	 flex: 0 0 calc(33.3333% - 1em);
+}
+ .Grid--holly-grail .aside, .Grid--holly-grail .main {
+	 flex: 1 100%;
+}
+/* One of -- columns*/
+ .Grid--1of2 > .Grid-cell, .Grid--1of4 > .Grid-cell:first-of-type, .Grid--1of3 > .Grid-cell:first-of-type {
+	 flex: 0 0 100%;
+}
+ .Grid--1of6 > .Grid-cell:first-of-type {
+	 flex: 0 0 50%;
+}
+ .Grid--fit > .Grid-cell {
+	 flex: 1;
+}
+ .Grid--full > .Grid-cell {
+	 flex: 0 0 100%;
+}
+/* Tablet (medium) screens */
+ @media (min-width: 30em) {
+	 .Grid--cols-4 > .Grid-cell {
+		 flex: 0 0 calc(50% - 1em);
+	}
+	 .Grid--cols-6 > .Grid-cell {
+		 flex: 0 0 calc(33.3333% - 1em);
+	}
+	 .Grid--cols-12 > .Grid-cell {
+		 flex: 0 0 calc(16.6666% - 1em);
+	}
+	 .Grid--holly-grail .aside {
+		 flex: 1 calc(25% - 1em);
+	}
+	 .Grid--1of2 > .Grid-cell {
+		 flex: 0 0 50%;
+	}
+	 .Grid--1of6 > .Grid-cell:first-of-type {
+		 flex: 0 0 30%;
+	}
+	 .Grid--1of4 > .Grid-cell:first-of-type {
+		 flex: 0 0 50%;
+	}
+	 .Grid--1of3 > .Grid-cell:first-of-type {
+		 flex: 0 0 100%;
+	}
+}
+/* Large screens */
+ @media (min-width: 48em) {
+	 .Grid--cols-2 > .Grid-cell, .Grid--cols-3 > .Grid-cell, .Grid--cols-4 > .Grid-cell, .Grid--cols-6 > .Grid-cell, .Grid--cols-12 > .Grid-cell {
+		 flex: 1;
+	}
+	 .Grid--holly-grail .main {
+		 flex: 2;
+	}
+	 .Grid--holly-grail .aside {
+		 flex: 1;
+	}
+	 .Grid--holly-grail .aside-1 {
+		 order: 1;
+	}
+	 .Grid--holly-grail .main {
+		 order: 2;
+	}
+	 .Grid--holly-grail .aside-2 {
+		 order: 3;
+	}
+	 .Grid--1of2 > .Grid-cell {
+		 flex: 0 0 50%;
+	}
+	 .Grid--1of6 > .Grid-cell:first-of-type {
+		 flex: 0 0 16.6666%;
+	}
+	 .Grid--1of4 > .Grid-cell:first-of-type {
+		 flex: 0 0 25%;
+	}
+	 .Grid--1of3 > .Grid-cell:first-of-type {
+		 flex: 0 0 30%;
+	}
+	 .Grid--gutters.Grid--nested .Grid-cell:first-of-type .Demo {
+		 margin-right: 0;
+	}
+}
+ .content-1of1::before {
+	 content: "1";
+}
+ .content-1of2::before {
+	 content: "1/2";
+}
+ .content-1of3::before {
+	 content: "1/3";
+}
+ .content-1of4::before {
+	 content: "1/4";
+}
+ .content-1of6::before {
+	 content: "1/6";
+}
+ .content-1of12::before {
+	 content: "1/12";
+}
+ 
+
+* {
+	box-sizing: border-box;
+}
+body {
+	font-family: 'Montserrat', sans-serif;
+	line-height: 1.6;
+	margin: 0;
+	min-height: 100vh;
+}
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+
+h2,
+h3,
+a {
+  color: #eaeef2;
+  text-align: center;
+}
+
+a {
+	text-decoration: none;
+}
+
+
+
+.logo {
+	margin: 0;
+	font-size: 1.45em;
+}
+
+.main-nav {
+  text-align: center;
+  margin: auto;
+
+}
+.logo a,
+.main-nav a {
+	padding: 10px 15px;
+	text-transform: uppercase;
+	text-align: center;
+	display: block;
+}
+
+.main-nav a {
+	color: #34495e;
+	font-size: .99em;
+}
+
+.main-nav a:hover {
+	color: #718daa;
+}
+
+
+
+.header {
+	padding-top: .5em;
+	padding-bottom: .5em;
+	border: 1px solid #a2a2a2;
+	background-color: #f4f4f4;
+	-webkit-box-shadow: 0px 0px 14px 0px rgba(0,0,0,0.75);
+	-moz-box-shadow: 0px 0px 14px 0px rgba(0,0,0,0.75);
+	box-shadow: 0px 0px 14px 0px rgba(0,0,0,0.75);
+	-webkit-border-radius: 5px;
+	-moz-border-radius: 5px;
+	border-radius: 5px;
+}
+
+
+/* ================================= 
+  Media Queries
+==================================== */
+
+
+
+
+@media (min-width: 769px) {
+	.header,
+	.main-nav {
+		display: flex;
+	}
+	.header {
+		flex-direction: column;
+		align-items: center;
+	}
+
+}
+
+@media (min-width: 1025px) {
+	.header {
+		justify-content: space-between;
+	}
+
+}
+
+
+/* TO LOGIN PAGE*/
+
+*{
+  -ms-box-sizing: border-box;
+	-moz-box-sizing: border-box;
+	-webkit-box-sizing: border-box;
+	box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+
+.login{
+  position: relative;
+  top: 50%;
+  width: 25rem;
+  text-align: center;
+  display: table;
+  margin: 50px auto 0 auto;
+  background: rgb(111, 108, 108);
+  border-radius: 4px;
+}
+
+.legend{
+  position: relative;
+  width: 100%;
+  display: block;
+  background: #FF7052;
+  padding: 15px;
+  color: #fff;
+  font-size: 20px;
+  
+}
+
+.input{
+  position: relative;
+  width: 90%;
+  margin: 2rem;
+  font-size:14px;
+
+  
+}
+
+.submit{
+  width: 45px;
+  height: 45px;
+  display: block;
+  margin: 0 auto -15px auto;
+  background: #fff;
+  border-radius: 100%;
+  border: 1px solid #FF7052;
+  color: #FF7052;
+  font-size: 24px;
+  cursor: pointer;
+  box-shadow: 0px 0px 0px 7px #fff;
+  transition: 0.2s ease-out;
+  
+
+}
+
+.feedback{
+  position: absolute;
+  bottom: -70px;
+  width: 100%;
+  text-align: center;
+  color: #fff;
+  background: #2ecc71;
+  padding: 10px 0;
+  font-size: 12px;
+  display: none;
+  opacity: 0;
+
+}
+
+.textarea {
+  height: 1.7rem;
+  padding: .5rem;
+}
+
+#topfeed {
+  text-align: center;
+}
+
+/* HEADER/NAVBAR */ 
+.main-nav {
+  text-align: center;
+  margin: auto;
+
+}
+.logo a,
+.main-nav a {
+	padding: 10px 15px;
+	text-transform: uppercase;
+	text-align: center;
+	display: block;
+}
+
+.main-nav a {
+	color: #34495e;
+	font-size: .99em;
+}
+
+.main-nav a:hover {
+	color: #718daa;
+}
+
+.header {
+	padding-top: .5em;
+	padding-bottom: .5em;
+	border: 1px solid #a2a2a2;
+	background-color: #f4f4f4;
+	-webkit-box-shadow: 0px 0px 14px 0px rgba(0,0,0,0.75);
+	-moz-box-shadow: 0px 0px 14px 0px rgba(0,0,0,0.75);
+	box-shadow: 0px 0px 14px 0px rgba(0,0,0,0.75);
+	-webkit-border-radius: 5px;
+	-moz-border-radius: 5px;
+	border-radius: 5px;
+}
+
+.header-banner {
+height:100px;
+background-image: url("images/Header.png");
+background-repeat: no-repeat;
+margin: auto;
+width: 30%;
+background-size: contain;
+
+    }
         </style>
+
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -79,9 +519,9 @@
                         <a href="{{ route('login') }}">Login</a>
 
                       @if(Route::has('category'))
-                      <a href="{{ route('fotbal')}}">Fotbal</a>
-                      <a href="{{ route('basebal')}}">Basebal</a>
-                      <a href="{{ route('basketbal')}}">Basketbal</a>
+                      <a href="{{ route('fotbal')}}">Fotbal</a> 
+                      <a href="{{ route('basebal')}}">Basebal</a>  
+                      <a href="{{ route('basketbal')}}">Basketbal</a>   
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
@@ -91,20 +531,99 @@
                 </div>
             @endif
 
+                <div class="header-banner">
+                  
+          </div>
+            <header class="header">
             <div class="content">
-                <div class="title m-b-md">
-                <h2> SportMob</h2>
-                <h6> Sports when ever you need it </h6>
-                </div>
+
 
                 <div class="links">
-                    <a href="http://localhost:8888/SportMob/public/football">Football</a>
-                    <a href="http://localhost:8888/SportMob/public/baseball">Baseball</a>
-                    <a href="http://localhost:8888/SportMob/public/basketball">Basketball</a>
+                <ul class="main-nav">
+                    <a href="./resources/fotbal.blade.php">Fotbal</a> 
+                    <a href="./resources/basebal.blade.php">Basebal</a>
+                    <a href="./resources/basketbal.blade.php">Basketbal</a>
                     <a href="http://localhost:8888/SportMob/public/articles">Articles</a>
                     <a href="http://localhost:8888/SportMob/public/private">ADMIN</a>
+                    </ul>
                 </div>
             </div>
+            </header> 
+
         </div>
+        <div class="wrapper">
+                        <h1 id="topfeed">Top feed</h1>
+                      <div class="Grid Grid--gutters Grid--Article-grail">
+                        <div class="Grid-cell main">
+
+                          <div class="Demo Article">
+                            <strong>Main</strong>
+                            <p>Basket</p>
+                          </div>
+
+                          <div class="Demo Article">
+                                <strong>Main</strong>
+                                <p>Basket</p>
+                              </div>
+                          <div class="Demo Article">
+                            <strong>Main</strong>
+                            <p>Basket</p>
+                          </div>
+
+                          <div class="Demo Article">
+                                <strong>Main</strong>
+                                <p>Basket</p>
+                              </div>
+                        </div>
+                        <div class="Grid-cell aside aside-1">
+                          <div class="Demo Article"><strong>Aside 1</strong><br />
+                            <div class="img-placeholder"></div>
+                            
+                            Fotball                           </div>
+
+                          <div class="Demo Article"><strong>Aside 1</strong><br />
+                            <div class="img-placeholder"></div>
+                            
+                            Fotball                           </div>
+
+                          <div class="Demo Article"><strong>Aside 1</strong><br />
+                            <div class="img-placeholder"></div>
+                            
+                            Fotball                           </div>
+
+                          <div class="Demo Article"><strong>Aside 1</strong><br />
+                            <div class="img-placeholder"></div>
+                            
+                           Fotball 
+                          </div>
+                        </div>
+                        <div class="Grid-cell aside aside-2">
+
+                          <div class="Demo Article">
+                            <strong>Aside 2</strong>
+                            <p>Baseball.</p>
+                          </div>
+
+                          <div class="Demo Article">
+                                <strong>Aside 2</strong>
+                                <p>Baseball.</p>
+                              </div>
+
+                              <div class="Demo Article">
+                                    <strong>Aside 2</strong>
+                                    <p>Baseball.</p>
+                                  </div>
+                                  <div class="Demo Article">
+                                        <strong>Aside 2</strong>
+                                        <p>Baseball.</p>
+                                      </div>
+                                      
+                        </div>
+                      </div>  
+                    
+</body>
+</html>
+                    
+        
     </body>
 </html>
